9be1f1abd028902b16a26eb0ffd94dda90e9632e
Aldi Setiawan <aldi.setiawan@dipostar.com>
2023-09-07 17:16:01 +0700

    adjustment config key to API_KEY_CHAT_GPT

--- app/config/config.py
+++ app/config/config.py
@@ -3,7 +3,7 @@ import os
 TITLE="Health Engine"
 DESCRIPTION="System that combines artificial intelligence (AI) techniques, specifically leveraging the capabilities of Pandas AI"
 
-OPENAI_API_TOKEN= os.getenv("OPENAI_API_TOKEN")
+API_KEY_CHAT_GPT= os.getenv("API_KEY_CHAT_GPT")
 
 #Mongo
 CONNECTION_MONGO = "mongodb://localhost:27017/"
--- app/engine/engine.py
+++ app/engine/engine.py
@@ -2,14 +2,14 @@ import openai
 from typing import List
 from fastapi.encoders import jsonable_encoder
 
-from app.config.config import OPENAI_API_TOKEN
+from app.config.config import API_KEY_CHAT_GPT
 from app.schema.chat import SchemaChats, SchemaChatResponse
 
 class ChatGPT:
     def __init__(self, chats: List[SchemaChats], model: str = "gpt-3.5-turbo"):
         self.chats = chats
         self.model = model
-        self.api_token = OPENAI_API_TOKEN
+        self.api_token = API_KEY_CHAT_GPT
 
     async def run(self):
         response = openai.ChatCompletion.create(
